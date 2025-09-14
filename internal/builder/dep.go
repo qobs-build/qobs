8a7299898e2a30677204aa34a53e990ebfbb6832
zeozeozeo <108888572+zeozeozeo@users.noreply.github.com>
2025-09-14 13:18:19 +0500

    allow for revision and branch in git url

--- internal/builder/dep.go
+++ internal/builder/dep.go
@@ -2,11 +2,13 @@ package builder
 
 import (
 	"errors"
+	"fmt"
 	"net/url"
 	"os"
 	"strings"
 
 	"github.com/go-git/go-git/v6"
+	"github.com/go-git/go-git/v6/plumbing"
 )
 
 var depShortcuts = map[string]string{
@@ -28,12 +30,12 @@ func fetchDependency(dep string, toWhere string) (string, error) {
 		return "", errIllegalDep
 	}
 
-	// check for `git:` prefix, e.g. git:https://github.com/zeozeozeo/qobs.git
+	// check for `git:` prefix, e.g. git:https://github.com/zeozeozeo/libhelloworld.git
 	if strings.HasPrefix(dep, gitPrefix) {
 		return cloneGitRepo(dep[len(gitPrefix):], toWhere)
 	}
 
-	// check for shortcut prefix, e.g. gh:zeozeozeo/qobs
+	// check for shortcut prefix, e.g. gh:zeozeozeo/libhelloworld
 	for shortcut, url := range depShortcuts {
 		if strings.HasPrefix(dep, shortcut) {
 			return cloneGitRepo(url+dep[len(shortcut):], toWhere)
@@ -54,15 +56,81 @@ func isURL(maybeURL string) bool {
 	return err == nil && u.Scheme != "" && u.Host != ""
 }
 
+type gitURL struct {
+	cleanURL    string
+	branch      string
+	commitOrTag string
+}
+
+// someone/something@master#0.1.0
+// someone/something@feature-branch#12345abc
+// someone/something#12345abc
+func parseGitURL(rawURL string) (res gitURL) {
+	parts := strings.SplitN(rawURL, "#", 2)
+	baseURL := parts[0]
+	if len(parts) == 2 {
+		res.commitOrTag = parts[1]
+	}
+
+	parts = strings.SplitN(baseURL, "@", 2)
+	res.cleanURL = parts[0]
+	if len(parts) == 2 {
+		res.branch = parts[1]
+	}
+
+	if !strings.HasSuffix(res.cleanURL, ".git") {
+		res.cleanURL += ".git"
+	}
+
+	return
+}
+
+// cloneGitRepo clones a Git remote into the specified directory
 func cloneGitRepo(url, toWhere string) (string, error) {
-	_, err := git.PlainClone(toWhere, &git.CloneOptions{
-		URL:               url,
+	parsedURL := parseGitURL(url)
+
+	cloneOptions := &git.CloneOptions{
+		URL:               parsedURL.cleanURL,
 		Progress:          os.Stdout,
-		Depth:             1,                                  // --depth=1
-		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth, // --recursive
-		ShallowSubmodules: true,                               // --shallow-submodules
-	})
-	return toWhere, err
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+	}
+
+	if parsedURL.commitOrTag == "" {
+		cloneOptions.Depth = 1 // we can do a shallow clone of the latest commit
+	}
+
+	if parsedURL.branch != "" {
+		cloneOptions.ReferenceName = plumbing.NewBranchReferenceName(parsedURL.branch)
+		cloneOptions.SingleBranch = true
+	}
+
+	repo, err := git.PlainClone(toWhere, cloneOptions)
+	if err != nil {
+		return toWhere, err
+	}
+
+	if parsedURL.commitOrTag != "" {
+		w, err := repo.Worktree()
+		if err != nil {
+			return toWhere, fmt.Errorf("could not get worktree: %w", err)
+		}
+
+		revision := parsedURL.commitOrTag
+		hash, err := repo.ResolveRevision(plumbing.Revision(revision))
+		if err != nil {
+			return toWhere, fmt.Errorf("could not resolve revision `%s`: %w", revision, err)
+		}
+
+		err = w.Checkout(&git.CheckoutOptions{
+			Hash:  *hash,
+			Force: true,
+		})
+		if err != nil {
+			return toWhere, fmt.Errorf("failed to checkout `%s`: %w", revision, err)
+		}
+	}
+
+	return toWhere, nil
 }
 
 func downloadAndExtractArchive(url, toWhere string) (string, error) {
